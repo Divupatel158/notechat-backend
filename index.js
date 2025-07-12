fd6a00ba7de83464bcc34c56abcf8d68aeb59dc0
Divupatel158 <kachhadiyadivyang158@gmail.com>
2025-07-12 12:32:10 +0530

    2nd

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 
 // CORS configuration - allow both 3000 and 3001 for development
 app.use(cors({ 
-  origin: ["http://localhost:3000", "http://localhost:3010"], 
+  origin: ["http://localhost:3010","https://NoteChat.onrender.com"], 
   credentials: true 
 }));
 app.use(express.json());
